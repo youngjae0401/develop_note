6e30168efd9a5f35d9a43e99106ca2c93aa810e2
Jo Youngjae <joyoungjae0401@gmail.com>
2022-06-07 00:35:20 +0900

    이미지 확대 스크립트 추가

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    let images = document.querySelectorAll('.image');
+    for(let i = 0; i < images.length; i++) {
+        images[i].addEventListener("click",imagePopup);
+    }
+
+    window.addEventListener("click",closePopup);
+};
+
+function imagePopup(e) {
+    let target = e.target;
+
+    document.querySelector('#popup .content img').src = target.getAttribute('src');
+    document.querySelector('body').classList.add('popup');
+}
+
+function closePopup(e) {
+    if(e.target === document.getElementById('popup')) {
+        document.querySelector('#popup .content img').src = "";
+        document.querySelector('body').classList.remove('popup');
+    }
+}
